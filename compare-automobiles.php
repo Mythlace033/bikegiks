6450bc9b5da68cd4430ff6dc0149bad458685749
Dambar-Shah <dambar981040@gmail.com>
2022-01-27 23:12:15 +0545

    commit on main

--- compare-automobiles.php
+++ compare-automobiles.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="widtd=device-widtd, initial-scale=1.0">
     <title>Bikegiks: Compare Automobiles</title>
     <?php
     include("assets/includes/links.php");
@@ -53,23 +53,23 @@
             <div class="col-md-12 p-2 mx-auto table-responsive">
                 <table class="table table-borderless table-striped">
                     <tbody>
+                        <!-- <tr>
+                            <td></td>
+                            <td class="text-center"><img src="assets\images\dominar-d400.png" style="widtd:200px" alt=""></td>
+                            <td class="text-center"><img src="assets\images\dominar-d400.png" style="widtd:200px" alt=""></td>
+                        </tr> -->
                         <tr>
-                            <th></th>
-                            <td class="text-center"><img src="assets\images\dominar-d400.png" style="width:200px" alt=""></td>
-                            <td class="text-center"><img src="assets\images\dominar-d400.png" style="width:200px" alt=""></td>
+                            <td>Name</td>
+                            <td><strong>Dominar D400</strong></td>
+                            <td><strong>Dominar D400</strong></td>
                         </tr>
                         <tr>
-                            <th>Name</th>
-                            <td>Dominar D400</td>
-                            <td>Dominar D400</td>
-                        </tr>
-                        <tr>
-                            <th>Performance</th>
+                            <td>Performance</td>
                             <td>25mm Max Torque, 373.3cc Engine Capacity, 40ps Engine Output, 13L full Tank, 184KGS kerb Weight </td>
                             <td>25mm Max Torque, 373.3cc Engine Capacity, 40ps Engine Output, 13L full Tank, 184KGS kerb Weight </td>
                         </tr>
                         <tr>
-                            <th>Engine</th>
+                            <td>Engine</td>
                             <td>Type: Liquid cooled, Single cylinder, DOHC
                                 Max Power: 39.9 PS @ 8650 rpm
                                 Max Torque: 35 @ 7000 (Nm @ RPM)
@@ -84,26 +84,26 @@
                             </td>
                         </tr>
                         <tr>
-                            <th>Vechile</th>
+                            <td>Vechile</td>
                             <td>Wheel Base: 1453 mm
-                                Length x Width x Height: 2156 mm x 836 mm x 1112 mm
+                                Lengtd x Widtd x Height: 2156 mm x 836 mm x 1112 mm
                                 Ground Clearance: 157 mm
                                 Suspension Front: USD Forks
                                 Kerb Weight: 184.5 Kg
                                 Fuel Tank (Reserve / Usable): 13 L
-                                Suspension Rear: Mono suspension with Nitrox
+                                Suspension Rear: Mono suspension witd Nitrox
                             </td>
                             <td>Wheel Base: 1453 mm
-                                Length x Width x Height: 2156 mm x 836 mm x 1112 mm
+                                Lengtd x Widtd x Height: 2156 mm x 836 mm x 1112 mm
                                 Ground Clearance: 157 mm
                                 Suspension Front: USD Forks
                                 Kerb Weight: 184.5 Kg
                                 Fuel Tank (Reserve / Usable): 13 L
-                                Suspension Rear: Mono suspension with Nitrox
+                                Suspension Rear: Mono suspension witd Nitrox
                             </td>
                         </tr>
                         <tr>
-                            <th>Breaks & Tyres</th>
+                            <td>Breaks & Tyres</td>
                             <td>Brake Size Front: Disc
                                 Brake Size Rear: Disc
                                 Tyre Front: 110/70- 17 Radial
@@ -120,16 +120,16 @@
                             </td>
                         </tr>
                         <tr>
-                            <th>Electrical</th>
+                            <td>Electrical</td>
                             <td>Battery: 12V, 8Ah VRLA
-                                Head Lamps: Full LED with Auto Headlamp On (AHO)
+                                Head Lamps: Full LED witd Auto Headlamp On (AHO)
                             </td>
                             <td>Battery: 12V, 8Ah VRLA
-                                Head Lamps: Full LED with Auto Headlamp On (AHO)
+                                Head Lamps: Full LED witd Auto Headlamp On (AHO)
                             </td>
                         </tr>
                         <tr>
-                            <th>Other</th>
+                            <td>Otder</td>
                             <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos amet rerum quam reiciendis, distinctio, facilis ducimus velit hic nostrum dignissimos, nulla facere veniam. Nemo alias, quisquam sed quam qui ipsum.
                             </td>
                             <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos amet rerum quam reiciendis, distinctio, facilis ducimus velit hic nostrum dignissimos, nulla facere veniam. Nemo alias, quisquam sed quam qui ipsum.
