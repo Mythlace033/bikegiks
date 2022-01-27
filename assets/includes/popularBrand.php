a9b9591e14a4dc8b4dd12633eae2d142a8351dba
Dambar-Shah <dambar981040@gmail.com>
2022-01-27 15:07:28 +0545

    commit on main

--- assets/includes/popularBrand.php
+++ assets/includes/popularBrand.php
@@ -1,37 +1,45 @@
 <style>
-
+    td img{
+        width: 200px;
+        min-width: 200px;
+    }
+    .table-responsive::-webkit-scrollbar{
+        display: none;
+    }
 </style>
 <div class="container my-5">
     <div class="row">
         <div class="col-md-12 p-3">
             <h2>Popular Brands</h2>
-            <table class="table table-responsive">
-                <tbody>
-                    <tr>
-                        <td>
-                            <img src="assets\images\rx1.jpg" alt="">
-                        </td>
-                        <td>
-                            <img src="assets\images\rx1.jpg" alt="">
-                        </td>
-                        <td>
-                            <img src="assets\images\rx1.jpg" alt="">
-                        </td>
-                        <td>
-                            <img src="assets\images\rx1.jpg" alt="">
-                        </td>
-                        <td>
-                            <img src="assets\images\rx1.jpg" alt="">
-                        </td>
-                        <td>
-                            <img src="assets\images\rx1.jpg" alt="">
-                        </td>
-                        <td>
-                            <img src="assets\images\rx1.jpg" alt="">
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <div class="table-responsive">
+                <table class="table">
+                    <tbody>
+                        <tr>
+                            <td>
+                                <img src="assets\images\rx1.jpg" alt="">
+                            </td>
+                            <td>
+                                <img src="assets\images\rx2.jpg" alt="">
+                            </td>
+                            <td>
+                                <img src="assets\images\rx3.jpg" alt="">
+                            </td>
+                            <td>
+                                <img src="assets\images\rx3.jpg" alt="">
+                            </td>
+                            <td>
+                                <img src="assets\images\rx3.jpg" alt="">
+                            </td>
+                            <td>
+                                <img src="assets\images\rx3.jpg" alt="">
+                            </td>
+                            <td>
+                                <img src="assets\images\rx3.jpg" alt="">
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>
