65a2d486d160d7ed0a9612bd626a290d6c53345c
Mith227 <dhanukyuba@gmail.com>
2022-01-29 08:19:22 +0545

    commit on main

--- news.php
+++ news.php
@@ -24,15 +24,15 @@
         </div>
         <div class="row my-3  p-2 m-3 shadow">
             <div class="col-md-4">
-                <a href="news-details.php"><img src="assets/images/dominar-d400.png" alt=""></a>
+                <a href="news-details.php"><img src="assets/images/hero-splendor-sales-dec-2021-breakup-747x420.jpg" alt=""></a>
             </div>
             <div class="col-md-6">
-                <a href="news-details.php" class="text-decoration-none text-danger"> <h2>Lorem ipsum dolor in. Lorem ipsum dolor in Lorem ipsum dolor in Lorem ipsum dolor </h1></a>
-                <a href="news-category.php?category=news"><span class="badge bg-danger">New</span></a> &nbsp; 
-                <strong>Arun Prakash</strong> &nbsp; 
-                <i>22-01-2022</i>
+                <a href="news-details.php" class="text-decoration-none text-danger"> <h2>Hero Sales, Exports Breakup Dec 2021 Splendor, Passion, Deluxe, Maestro.</h1></a>
+                <a href="news-category.php?category=news"><span class="badge bg-danger">Hero</span></a> &nbsp; 
+                <strong>Bikegiks</strong> &nbsp; 
+                <i>JANUARY 27, 2022</i>
                 <br> <br>
-                <p><strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo architecto praesentium sed, et aperiam eos eligendi vitae nam velit sapiente totam at sequi rem voluptate ut error ipsa quod a.</strong></p>
+                <p><strong>Hero MotoCorp suffered a YoY de-growth both in terms of domestic sales and exports in December 2021.</strong></p>
             </div>           
         </div>
         <div class="row my-3  p-2 m-3 shadow">
